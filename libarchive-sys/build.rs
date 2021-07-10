c0c930bc4c165474f00db9f10d5cc290c7599775
Muhammad Fadhlika <fadhlika@gmail.com>
2021-07-10 15:30:40 +0700

    refactor

--- libarchive-sys/build.rs
+++ libarchive-sys/build.rs
@@ -77,7 +77,22 @@ fn link_libarchive(_mode: &str) {
 
 #[cfg(not(target_env = "msvc"))]
 fn link_libarchive(mode: &str) {
-    println!("cargo:rustc-link-lib={}=archive", mode);
+    if cfg!(feature = "vendored") {
+        println!("cargo:rustc-link-lib={}=archive", mode);
+    } else {
+        let libarchive = pkg_config::Config::new()
+            .statik(mode == "static")
+            .probe("libarchive")
+            .expect("Unable to find libarchive");
+
+        for link_path in libarchive.link_paths {
+            println!("cargo:rustc-link-search=native={}", link_path.display());
+        }
+
+        for lib in libarchive.libs {
+            println!("cargo:rustc-link-lib={}={}", mode, lib);
+        }
+    }
 }
 
 #[cfg(target_env = "msvc")]
@@ -104,6 +119,21 @@ fn link_deps(_mode: &str) {
 
 #[cfg(not(target_env = "msvc"))]
 fn link_deps(mode: &str) {
+    find_link_paths();
+    
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-search=native=/usr/local/opt/bzip2/lib");
+
+    #[cfg(target_os = "macos")]
+    link_expat(mode);
+
+    link_icuuc(mode);
+}
+
+fn find_link_paths() {
     let pc_path = pkg_config::get_variable("pkg-config", "pc_path").expect("failed to get pc_path");
 
     for path in pc_path.split(":") {
@@ -121,10 +151,24 @@ fn link_deps(mode: &str) {
             );
         }
     }
+}
 
-    #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-lib=dylib=stdc++");
+fn link_expat(mode: &str) {
+    let expat = pkg_config::Config::new()
+        .statik(mode == "static")
+        .probe("expat")
+        .expect("Unable to find expat");
 
+    for link_path in expat.link_paths {
+        println!("cargo:rustc-link-search=native={}", link_path.display());
+    }
+
+    for lib in expat.libs {
+        println!("cargo:rustc-link-lib={}={}", mode, lib);
+    }
+}
+
+fn link_icuuc(mode: &str) {
     let icu_uc = pkg_config::Config::new()
         .statik(mode == "static")
         .probe("icu-uc")
@@ -138,35 +182,6 @@ fn link_deps(mode: &str) {
         if lib == "pthread" || lib == "m" {
             continue;
         }
-        println!("cargo:rustc-link-lib={}={}", mode, lib);
-    }
-
-    let expat = pkg_config::Config::new()
-        .statik(mode == "static")
-        .probe("expat")
-        .expect("Unable to find expat");
-
-    for link_path in expat.link_paths {
-        println!("cargo:rustc-link-search=native={}", link_path.display());
-    }
-
-    for lib in expat.libs {
-        println!("cargo:rustc-link-lib={}={}", mode, lib);
-    }
-
-    let libarchive = pkg_config::Config::new()
-        .statik(mode == "static")
-        .probe("libarchive")
-        .expect("Unable to find libarchive");
-
-    for link_path in libarchive.link_paths {
-        println!("cargo:rustc-link-search=native={}", link_path.display());
-    }
-
-    #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-search=native=/usr/local/opt/bzip2/lib");
-
-    for lib in libarchive.libs {
         println!("cargo:rustc-link-lib={}={}", mode, lib);
     }
 }
